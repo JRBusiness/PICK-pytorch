8b410e01de95a13d340662978495de671e6e9714
JRBusiness <43971633+JRBusiness@users.noreply.github.com>
2022-08-25 17:44:06 -0400

    a

--- utils/entities_list.py
+++ utils/entities_list.py
@@ -3,10 +3,30 @@
 # @Created Time: 7/8/2020 9:34 PM
 
 Entities_list = [
-    "company",
-    "address",
-    "date",
-    "total"
+    "PROVIDER_NAME",
+    "MEM_NAME",
+    "MEM_ID",
+    "GROUP_NUMBER",
+    "RXBIN",
+    "RXPCN",
+    "ISSUER_ID",
+    "RXGROUP",
+    "EFFECTIVE",
+    "PLAN_CODE",
+    "DEPENDENTS",
+    "HEALTH_PLAN",
+    "DOB",
+    "PAYER_ID",
+    "COVERAGE_DATE",
+    "OUT_OF_POCKET",
+    "SUBCRIBER_ID",
+    "PCP",
+    "RXID",
+    "SUBSCRIBER_NAME",
+    "RX_PLAN",
+    "NETWORK",
+    "PEDIATRIC_MEMBER_DENTAL",
+    "POLICY_NUMBER",
 ]
 
 # Entities_list = [
